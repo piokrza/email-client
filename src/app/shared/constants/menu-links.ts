64020b8ec4f65cac0c39a355008e8fa384c2cb5d
Piotr Krzaczkowski <piotr.krzaczkowski@embiq.com>
2022-09-18 14:49:00 +0200

    add active link

--- src/app/shared/constants/menu-links.ts
+++ src/app/shared/constants/menu-links.ts
@@ -1,21 +1,27 @@
 import { MenuItem } from 'primeng/api';
 import { MenuLinkLabel } from '@shared/enums/menu-link-label.enum';
 
+//TODO: handle active links
+
 export const MenuLinks: MenuItem[] = [
   {
     label: MenuLinkLabel.INBOX,
     routerLink: '/inbox',
+    routerLinkActiveOptions: 'active',
   },
   {
     label: MenuLinkLabel.SIGN_OUT,
     routerLink: '/signout',
+    routerLinkActiveOptions: 'active',
   },
   {
     label: MenuLinkLabel.SIGN_IN,
     routerLink: '',
+    routerLinkActiveOptions: 'active',
   },
   {
     label: MenuLinkLabel.SIGN_UP,
     routerLink: '/signup',
+    routerLinkActiveOptions: 'active',
   },
 ];
