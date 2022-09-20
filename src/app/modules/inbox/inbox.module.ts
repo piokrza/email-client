e0afe677a332be68da34e2bd8127ebd40a05ea78
Piotr Krzaczkowski <piotr.krzaczkowski@embiq.com>
2022-09-20 20:22:46 +0200

    add email components, add email service

--- src/app/modules/inbox/inbox.module.ts
+++ src/app/modules/inbox/inbox.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InboxRoutingModule } from '@inbox/inbox-routing.module';
 import { HomeComponent } from '@inbox/components/home/home.component';
+import { EmailCreateComponent } from './components/email-create/email-create.component';
+import { EmailIndexComponent } from './components/email-index/email-index.component';
+import { EmailReplyComponent } from './components/email-reply/email-reply.component';
+import { EmailShowComponent } from './components/email-show/email-show.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, EmailCreateComponent, EmailIndexComponent, EmailReplyComponent, EmailShowComponent],
   imports: [CommonModule, InboxRoutingModule],
 })
 export class InboxModule {}
