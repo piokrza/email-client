979af0a2069925c3f926f693b8fa0f64e7ff8b41
Piotr Krzaczkowski <piotr.krzaczkowski@embiq.com>
2022-09-27 00:12:51 +0200

    add form container component, set username on signin

--- src/app/shared/components/signout/signout.component.ts
+++ src/app/shared/components/signout/signout.component.ts
@@ -1,26 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@auth/services/auth.service';
+import { DestroyComponent } from '@standalone/components/destroy/destroy.component';
+import { takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-signout',
   templateUrl: './signout.component.html',
   styleUrls: ['./signout.component.scss'],
 })
-export class SignoutComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+export class SignoutComponent extends DestroyComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {
+    super();
+  }
 
   ngOnInit(): void {
     this.redirectToMainPage();
   }
 
   redirectToMainPage(): void {
-    this.authService.signOut().subscribe({
-      next: () => {
-        setTimeout(() => {
-          this.router.navigateByUrl('/');
-        }, 5000);
-      },
-    });
+    this.authService
+      .signOut()
+      .pipe(
+        tap(() => {
+          setTimeout(() => {
+            this.router.navigateByUrl('/');
+          }, 3500);
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
   }
 }
